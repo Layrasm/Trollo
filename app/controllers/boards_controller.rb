76c23e60d132911beb1da494a7fc95eea7364c91
Layne Rasmussen <layrasm@gmail.com>
2020-09-03 09:19:17 -0600

    Added a new board view, couldn't get it all to work from index view

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,10 +1,36 @@
 class BoardsController < ApplicationController
+  before_action :set_board, only: [:show, :edit, :update]
+  
   def index
+    @boards = current_user.boards
   end
-
+  
   def show
   end
 
   def new
+    @board = Board.new
+  end
+
+  def create
+    @board = current_user.boards.new(board_params)
+
+    if @board.save
+      redirect_to boards_path
+    else 
+      render :new
+    end
+  end
+  
+  
+  private
+  
+  def set_board
+    @board = current_user.boards.find(params[:id])
+  end
+
+
+  def board_params
+    params.require(:board).permit(:name)
   end
 end
--- app/controllers/home_controller.rb
+++
@@ -1,7 +0,0 @@
-class HomeController < ApplicationController
-  def index
-    @boards = Board.all
-    @lists = List.all
-    @tasks = Task.all
-  end
-end
